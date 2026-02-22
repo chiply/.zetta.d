0433905f3a430699d4220a446524e3418d939605
Charlie Holland <mister.chiply@gmail.com>
2026-02-22 18:25:13 -0500

    feat: add system dependency guards to use-package declarations
    Prevent installation and loading of packages that wrap system binaries
    when those binaries are absent, ensuring clean startup on machines
    without ag, rg, docker, gh, aspell, tokei, mmdc, dot, or ImageMagick.

--- modules/tools/docker-compose-mode.el
+++ modules/tools/docker-compose-mode.el
@@ -1,4 +1,5 @@
 ;;; docker-compose-mode.el --- Configure docker-compose-mode -*- lexical-binding: t; -*-
 
-(use-package docker-compose-mode)
+(use-package docker-compose-mode
+  :if (executable-find "docker"))
 ;;; docker-compose-mode.el ends here

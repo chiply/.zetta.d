6a97a32ffc2b6f8e320a942044491296873fcf8d
Charlie Holland <mister.chiply@gmail.com>
2026-03-01 15:05:39 -0500

    refactor: use space-tree convenience commands instead of lambdas
    Replace 20 inline lambdas in spacetree keybindings with named
    commands from space-tree (space-tree-to-*, space-tree-sub-*,
    space-tree-sub-sub-*, space-tree-delete-space).

--- modules/ui/spacetree.el
+++ modules/ui/spacetree.el
@@ -9,33 +9,33 @@
   (unless (ht-keys space-tree-tree) (space-tree-init))
 
   :general
-  ("s-1" (lambda () (interactive) (space-tree-switch-or-create '(1)))
-   "s-2" (lambda () (interactive) (space-tree-switch-or-create '(2)))
-   "s-3" (lambda () (interactive) (space-tree-switch-or-create '(3)))
-   "s-4" (lambda () (interactive) (space-tree-switch-or-create '(4)))
-   "s-5" (lambda () (interactive) (space-tree-switch-or-create '(5)))
-   "s-6" (lambda () (interactive) (space-tree-switch-or-create '(6)))
-   "s-7" (lambda () (interactive) (space-tree-switch-or-create '(7)))
-   "s-8" (lambda () (interactive) (space-tree-switch-or-create '(8)))
-   "s-9" (lambda () (interactive) (space-tree-switch-or-create '(9)))
+  ("s-1" 'space-tree-to-1
+   "s-2" 'space-tree-to-2
+   "s-3" 'space-tree-to-3
+   "s-4" 'space-tree-to-4
+   "s-5" 'space-tree-to-5
+   "s-6" 'space-tree-to-6
+   "s-7" 'space-tree-to-7
+   "s-8" 'space-tree-to-8
+   "s-9" 'space-tree-to-9
    ;; Second level
-   "s-a" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) 1)))
-   "s-s" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) 2)))
-   "s-d" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) 3)))
-   "s-f" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) 4)))
-   "s-g" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) 5)))
+   "s-a" 'space-tree-sub-1
+   "s-s" 'space-tree-sub-2
+   "s-d" 'space-tree-sub-3
+   "s-f" 'space-tree-sub-4
+   "s-g" 'space-tree-sub-5
    ;; Third level
-   "s-A" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) ,(nth 1 space-tree-current-address) 1)))
-   "s-S" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) ,(nth 1 space-tree-current-address) 2)))
-   "s-D" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) ,(nth 1 space-tree-current-address) 3)))
-   "s-F" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) ,(nth 1 space-tree-current-address) 4)))
-   "s-G" (lambda () (interactive) (space-tree-switch-or-create `(,(nth 0 space-tree-current-address) ,(nth 1 space-tree-current-address) 5)))
+   "s-A" 'space-tree-sub-sub-1
+   "s-S" 'space-tree-sub-sub-2
+   "s-D" 'space-tree-sub-sub-3
+   "s-F" 'space-tree-sub-sub-4
+   "s-G" 'space-tree-sub-sub-5
    ;; Navigation
    "M-S-<tab>" 'space-tree-switch-space-by-name
    "M-<tab>" 'space-tree-go-to-last-space
    "C-M-<tab>" 'space-tree-go-right
    "C-M-S-<tab>" 'space-tree-go-left
-   "s-_" (lambda () (interactive) (space-tree-delete-space space-tree-current-address)))
+   "s-_" 'space-tree-delete-space)
   :general
   (:states '(normal visual)
    :keymaps 'override
